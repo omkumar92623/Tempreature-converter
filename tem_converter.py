b42d137e9796c4f0f2a3da7f97f58f2c9faa20f2
Om kumar <omkumar92623@gmail.com>
2026-05-24 11:20:48 +0530

    Refactor temperature converter with f-strings

--- tem_converter.py
+++ tem_converter.py
@@ -6,14 +6,14 @@ choice = int(input("enter what you want to do: "))
 
 match choice:
     case 1:
-        celsius=int(input("enter tempreature in Celsius "))
+        celsius=int(input("enter tempereature in Celsius "))
         fahrenheit=(celsius*9/5)+32
-        print("the Fahrenheit tempreature is ",fahrenheit)
+        print(f"the Fahrenheit tempereature is {fahrenheit}")
     
     case 2:
-        fahrenheit=float(input("enter tempreature in fahrenheit: "))
+        fahrenheit=float(input("enter tempereature in fahrenheit: "))
         celsius=(fahrenheit-32)*5/9
-        print("The Celsius tempreature is ",celsius)
+        print(f"The Celsius tempereature is {celsius}")
 
     case _:
         print("you enter wrong number")
